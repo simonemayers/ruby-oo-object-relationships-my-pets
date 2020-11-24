b405b29befae8449ab26ec149b74f1c4603021df
simonemayers <simone21295@gmail.com>
2020-11-24 11:28:32 -0500

    commit tree 8ebc6bbebaf2fc5504b58568a62bb2de63b38b0a

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,26 @@
 class Cat
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name, :owner
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    save
+  end
+
+  def save
+    @@all << self
+  end
+  
+  def owner=(owner)
+    @owner = owner
+  end
+
+  def self.all
+    @@all
+  end
+
 end
